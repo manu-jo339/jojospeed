a3b9e934b2fb643646047b56e928e251f07fdde7
manu-jo339 <emmanueltsingambia6@gmail.com>
2026-01-29 21:07:51 +0100

    Update config.js

--- web/public/config.js
+++ web/public/config.js
@@ -1,4 +1,4 @@
 // JojoSpeed config
 // After deploying your API on Render, replace this with your API URL, e.g.:
 // window.JOJO_API_BASE = "https://jojospeed-api.onrender.com";
-window.JOJO_API_BASE = "http://localhost:3000";
+window.JOJO_API_BASE = "https://jojospeed-api.onrender.com";
